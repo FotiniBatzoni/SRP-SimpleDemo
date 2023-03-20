3c1611e47ea5745b13ba2d128c0d3739a6d94a71
fotini <f.batzoni@gmail.com>
2023-03-20 15:20:54 +0200

    Add project files.

---
+++ SRPDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using SRPDemo;
+
+class Program
+{
+    static void Main()
+    {
+        StandardMessages.WelcomeMessage();
+
+        Person user = PersonDataCapture.Capture();
+
+        bool isUserValid = PersonValidator.Validate(user);
+
+        if (!isUserValid)
+        {
+            StandardMessages.EndApplication();
+            return;
+        }
+
+        AccountGenerator.CreateAccount(user);
+
+        StandardMessages.EndApplication();
+    }
+}
+
